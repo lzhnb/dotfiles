160bda8ff86b3c9cbf45537d8c81979bcdda34e8
lzhnb <eezhihaoliang@mail.scut.edu.cn>
2021-10-10 16:18:14 +0800

    add the funcs of fzf to realize kill process and fast tmux

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,7 +1,7 @@
 # source fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-# setting
+# ------------------------------------- settings ---------------------------------
 # set the height and preview
 export FZF_DEFAULT_OPTS="--height 50% --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
 
@@ -16,5 +16,39 @@ build,\
 __pycache__,\
 *.pyc\
 } --type f"
+
+# ------------------------------------- functions ---------------------------------
+# fkill - kill process
+fkill() {
+  local pid
+  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+	echo $pid | xargs kill -${1:-9}
+  fi
+}
+
+# tm - create new tmux session, or switch to existing one. Works from within tmux too. (@bag-man)
+# `tm` will allow you to select your tmux session via fzf.
+# `tm irc` will attach to the irc session (if it exists), else it will create it.
+
+tm() {
+  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+  if [ $1 ]; then
+	tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+  fi
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+}
+
+# fs [FUZZY PATTERN] - Select selected tmux session
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fs() {
+  local session
+  session=$(tmux list-sessions -F "#{session_name}" | \
+    fzf --query="$1" --select-1 --exit-0) &&
+  tmux switch-client -t "$session"
+}
 
 
